990d637b78b7381ab534614c2e3ba774864daf7c
Sylvester Keil <sylvester@keil.or.at>
2012-08-09 13:48:10 +0200

    v0.3.0
    [ci skip]

--- lib/namae/version.rb
+++ lib/namae/version.rb
@@ -1,8 +1,8 @@
 module Namae
   module Version
     MAJOR = 0
-    MINOR = 2
-    PATCH = 1
+    MINOR = 3
+    PATCH = 0
     BUILD = nil
 
     STRING = [MAJOR, MINOR, PATCH, BUILD].compact.join('.').freeze
